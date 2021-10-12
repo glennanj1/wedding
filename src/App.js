8e267d8d0fbe97b718ff11f18c717662f7144c84
John Glennan <glennan20@gmail.com>
2021-10-12 00:57:49 -0400

    added sound and some styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import Typewriter from 'typewriter-effect';
+import Sound from 'react-sound';
+
 
 
 function App() {
+  
   return (
     <div className="App">
       <video className="video" autoPlay loop playsInline defaultMuted muted >
         <source src="https://d3ddatyom1hv87.cloudfront.net/Wedding.mp4" type="video/mp4" />
       </video> 
+      <Sound
+      url="https://d3ddatyom1hv87.cloudfront.net/oneKiss.mp3"
+      playStatus={Sound.status.PLAYING}
+      volume={100}
+      />
       <Typewriter
         options={{
           strings: ['We are getting married!', 'Please RSVP below', 'Thanks for visiting the site!', "Who's excited?"],
