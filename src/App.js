7eba6b6327a199d5dd72cf2a0b55c2b8a55d9dfe
John Glennan <glennan20@gmail.com>
2021-10-11 22:26:38 -0400

    added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Typewriter from 'typewriter-effect';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video className="video" autoPlay loop playsInline defaultMuted muted >
+        <source src="https://d3ddatyom1hv87.cloudfront.net/Wedding.mp4" type="video/mp4" />
+      </video> 
+      <Typewriter
+        options={{
+          strings: ['We are getting married!', 'Please RSVP below', 'Thanks for visiting the site!', "Who's excited?"],
+          autoStart: true,
+          loop: true,
+        }}
+      />
+      <button className="rsvp-button">RSVP</button>
     </div>
   );
 }
