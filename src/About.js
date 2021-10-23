cf6ddd2e3a90d917f6c4f218b56ebbdf250b1ec5
glennanj1 <glennan20@gmail.com>
2021-10-23 01:05:19 -0400

    about page with css

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './About.css'
+
+function About() {
+    return (
+        <div className="container">
+            <h1>John & Victoria</h1>
+            <h2>10.29.2022</h2>
+        </div>
+    )
+}
+
+export default About
