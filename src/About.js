240ae575bb3ccba9f29a214e7a92f5e50dfd8d38
glennanj1 <glennan20@gmail.com>
2021-10-23 10:59:04 -0400

    changed audio to play on about page

--- src/About.js
+++ src/About.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './About.css'
+import 'animate.css';
+import Sound from 'react-sound';
 
 function About() {
+    
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    useEffect(() => {
+        // Update the document title using the browser API
+        setIsPlaying(true)
+    }, []);
+
     return (
-        <div className="container">
+        <div className="container animate__animated animate__slideInRight">
+            <Sound
+                url="https://d3ddatyom1hv87.cloudfront.net/oneKiss.mp3"
+                playStatus={isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED}
+                volume={50}
+                />
             <h1>John & Victoria</h1>
             <h2>10.29.2022</h2>
         </div>
