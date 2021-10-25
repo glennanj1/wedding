03e3e022c7461c06354841aa168c498dfddfd56f
glennanj1 <glennan20@gmail.com>
2021-10-25 16:40:04 -0400

    added a location component

---
+++ src/Location.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'animate.css';
+
+
+function Location() {
+    return (
+        <div className="container animate__animated animate__slideInLeft">
+            <h1>Location</h1>
+            <h2>The Farm: Bakery & Events</h2>
+        </div>
+    )
+}
+
+export default Location
