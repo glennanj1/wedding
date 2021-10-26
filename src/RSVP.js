d8f0beabf6aa321e552fed74bc1dbb43326f04c6
glennanj1 <glennan20@gmail.com>
2021-10-25 23:23:38 -0400

    functionality coming soon

---
+++ src/RSVP.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function RSVP() {
+    return (
+        <div className="container animate__animated animate__slideInLeft">
+            <h1>RSVP</h1>
+            <h2>Working on RSVP functionality 💒 ❤️</h2>
+        </div>
+    )
+}
+
+export default RSVP
