e781c479b41fc3ad1cbc8ebec6ae191661844c4b
glennanj1 <glennan20@gmail.com>
2021-10-25 23:22:17 -0400

    new component

---
+++ src/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutUs() {
+    return (
+        <div className="container animate__animated animate__slideInLeft">
+            <h1>Our Story</h1>
+            <h2>It was all a dream until it wasn't</h2>
+        </div>
+    )
+}
+
+export default AboutUs
