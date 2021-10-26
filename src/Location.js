0b678f941449e2c51cc35ee9de3e4cc4a73783c8
glennanj1 <glennan20@gmail.com>
2021-10-25 23:22:39 -0400

    added gmaps embed + changed wording

--- src/Location.js
+++ src/Location.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import 'animate.css';
+import './Location.css'
 
 
 function Location() {
     return (
-        <div className="container animate__animated animate__slideInLeft">
-            <h1>Location</h1>
-            <h2>The Farm: Bakery & Events</h2>
+        <div className="container map animate__animated animate__slideInLeft">
+            <h1>Google Maps</h1>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3035.054284542485!2d-75.3850087846064!3d40.47406406007443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c422fba0ceae9b%3A0x5425b64b2ed2ab8b!2sThe%20Farm%20Bakery%20%26%20Events!5e0!3m2!1sen!2sus!4v1635215889492!5m2!1sen!2sus" className="gMap" allowFullScreen="true" title="GoogleMaps" loading="lazy"></iframe>
         </div>
     )
 }
