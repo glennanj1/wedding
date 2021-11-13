f87b3a5366284e115d76a7f28f783647b6b0a0d1
glennanj1 <glennan20@gmail.com>
2021-11-12 22:27:59 -0500

    changed about us

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -3,8 +3,10 @@ import React from 'react'
 function AboutUs() {
     return (
         <div className="container animate__animated animate__slideInLeft">
-            <h1>Our Story</h1>
-            <h2>It was all a dream until it wasn't</h2>
+            <h1>Our Story:</h1>
+            <h2>Love at first site </h2>
+            <h2>This is what you came for</h2>
+            <h2>La Salle University </h2>
         </div>
     )
 }
