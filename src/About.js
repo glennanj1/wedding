9ba61017a92f2015b232ce305c44420b97a7b1c3
glennanj1 <glennan20@gmail.com>
2022-01-16 23:37:00 -0500

    added registry and loading state with spinner

--- src/About.js
+++ src/About.js
@@ -6,16 +6,32 @@ import Us from './Us'
 import AboutUs from './AboutUs';
 import RSVP from './RSVP';
 import Location from './Location';
+import Registry from './Registry';
 // import Sound from 'react-sound';
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 function About() {
 
+
+    //set isLoading
+    const [isLoading, setIsLoading] = useState(false);
     const [isDefault, setIsDefault] = useState(true);
     const [isLocation, setIsLocation] = useState(false);
     const [isAbout, setIsAbout] = useState(false);
     const [isRSVP, setIsRSVP] = useState(false)
+    const [isRegistry, setIsRegistry] = useState(false)
+    const [isClicked, setIsClicked] = useState('')
 
-
+    const handleError = e => {
+      if (e === 'none') {
+        setIsLoading(false);
+      } else {
+        setIsLoading(true)
+        console.log(e);
+        console.log('error line 25 prop')
+      }
+    }
 
     const handleClick = (e) => {
       console.log(e.currentTarget.id);
@@ -25,24 +41,36 @@ function About() {
           setIsLocation(true);
           setIsAbout(false);
           setIsRSVP(false)
+          setIsRegistry(false);
           break;
         case 'home':
           setIsDefault(true);
           setIsLocation(false);
           setIsAbout(false);
           setIsRSVP(false);
+          setIsRegistry(false);
           break;
         case 'about':
           setIsDefault(false);
           setIsLocation(false);
           setIsAbout(true);
           setIsRSVP(false);
+          setIsRegistry(false);
           break;
         case 'rsvp':
           setIsDefault(false);
           setIsLocation(false);
           setIsAbout(false);
           setIsRSVP(true);
+          setIsRegistry(false);
+          setIsClicked(e);
+          break;
+        case 'registry':
+          setIsDefault(false);
+          setIsLocation(false);
+          setIsAbout(false);
+          setIsRSVP(false);
+          setIsRegistry(true);
           break;
         default:
           setIsDefault(true);
@@ -56,12 +84,12 @@ function About() {
     return (
     <>
     <div className="animate__animated animate__fadeIn aboutWrapper">
-
       <div className="nav">
         <button onClick={handleClick} className="navButton" id="home">Home</button>
         <button onClick={handleClick} className="navButton" id="location">Location</button>
         <button onClick={handleClick} className="navButton" id="rsvp">RSVP</button>
         <button onClick={handleClick} className="navButton" id="about">Our Story</button>
+        <button onClick={handleClick} className="navButton" id="registry">Registry</button>
       </div>
 
       {isDefault ? ( 
@@ -75,11 +103,24 @@ function About() {
       {isAbout ? (
         <AboutUs />
       ) : null}
-
-      {isRSVP ? (
-        <RSVP />
+      
+      {isLoading ? 
+        (
+          <div className="container">
+            <CircularProgress />)
+          </div>
+        )
+      :  
+      isRSVP ? (
+        <RSVP handleCallback={handleError} passClickDown={isClicked} />
+      ) 
+      : 
+      null}
+      {isRegistry ? (
+        <div className="container">
+          <Registry /> 
+        </div>
       ) : null}
-
       <SimpleBottomNavigation handleCallback={handleClick} />
     </div>
     </>
