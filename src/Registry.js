80559e0ddbddd88c7bfd1024bdabca97dcf8ce61
glennanj1 <glennan20@gmail.com>
2022-01-16 23:40:54 -0500

    Tentative registry

---
+++ src/Registry.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Registry() {
+    return (
+            <h1>
+                <a href="https://www.target.com/gift-registry/gift/the-future-glennans">Target</a>
+            </h1>
+    )
+}
