103dd1727b533e35254a67b9e091d5542a80571c
glennanj1 <glennan20@gmail.com>
2022-05-07 13:23:55 -0400

    about us

--- src/Containers/AboutUs.js
+++ src/Containers/AboutUs.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Nav from '../Containers/Nav'
+import '../Styles/AboutUs.css'
 
 function AboutUs() {
     return (
         <>
         <Nav />
-        <div className="container animate__animated animate__slideInLeft">
-            <h1>Our Story Coming Soon</h1>
+        <div className="aboutUs animate__animated animate__slideInLeft">
+            <main className="aboutUsContainer">
+                <h1>
+                How It All Began- 
+                </h1>
+                <p>
+                John and Victoria first met back in 2016 at their Alma Madar, La Salle University. Their love story didn’t start there though. A few weeks prior to their first official time meeting/talking, John and Victoria ran into each other at a local bar. While they were not officially introduced at this bar, Victoria remembers saying to her friend “look at that cute guy over there”. Little did Victoria know that would one day become her husband. Weeks later they officially met during an end of year gathering at La Salle. Victoria was shocked to again see that cute guy from the bar, DJing at the gathering. They hit it off right away and the rest is history! 
+                </p>
+                <p>
+                During their time after college, John and Victoria spent time exploring different areas and moving around (a lot). One of their favorite memories is the year they spent living up in The Poconos together at the start of the pandemic. But their most favorite memory is the day they got their first puppy together, a goldendoodle named Ozzy, back in November 2019! 
+                </p>
+                <p>
+                John proposed to Victoria on the morning of New Years Eve- 12/31/2019. John proposed at a local park in Roxborough, PA, which was down the street from the apartment that they were living in at the time. Victoria, John, and Ozzy would often take walks in this park together and talk about their future and all the exciting things that they had to look forward to together.   
+                </p>
+                <p>
+                After 6.5 years together, we are so excited to celebrate our wedding day with our friends and family!  
+                </p>
+            </main>
         </div>
         </>
     )
