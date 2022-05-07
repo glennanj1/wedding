0c65bb14742b964517529e84c24421df4f364ee9
glennanj1 <glennan20@gmail.com>
2022-05-07 13:24:52 -0400

    registry links

--- src/Containers/Registry.js
+++ src/Containers/Registry.js
@@ -7,9 +7,12 @@ export default function Registry() {
     return (
         <>
             <Nav />
-            <div className="container animate__animated animate__slideInLeft">
-                <h1>
-                    Coming Soon
+            <div className="registryContainer animate__animated animate__slideInLeft">
+                <h1 className="link">
+                    <a href="https://www.target.com/gift-registry/gift/the-future-glennans" alt="target">Target</a>
+                </h1>
+                <h1 className="link">
+                    <a href="https://www.amazon.com/wedding/victoria-dangelo-john-glennan--october-2022/registry/3JIFEPXJ8PHZN" alt="amazon">Amazon</a>
                 </h1>
             </div>
         </>
