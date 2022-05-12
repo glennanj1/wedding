d16d6ca09628b6b72718a0c118db19e32921bf2a
glennanj1 <glennan20@gmail.com>
2022-05-12 15:14:48 -0400

    updates

--- src/Containers/Registry.js
+++ src/Containers/Registry.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import Nav from '../Containers/Nav'
 import '../Styles/Registry.css'
+import target from '../Images/target.png'
+import amazon from '../Images/amazon.png'
 
 export default function Registry() {
     //href="https://www.target.com/gift-registry/gift/the-future-glennans"
     return (
         <>
             <Nav />
-            <div className="registryContainer animate__animated animate__slideInLeft">
-                <h1 className="link">
-                    <a href="https://www.target.com/gift-registry/gift/the-future-glennans" alt="target">Target</a>
-                </h1>
-                <h1 className="link">
-                    <a href="https://www.amazon.com/wedding/victoria-dangelo-john-glennan--october-2022/registry/3JIFEPXJ8PHZN" alt="amazon">Amazon</a>
-                </h1>
-            </div>
+                <div className="registryContainer animate__animated animate__slideInLeft">
+                    <h1 className="link">
+                        <a href="https://www.target.com/gift-registry/gift/the-future-glennans" alt="t"><img src={target} style={{width: "200px", height: "250px"}} alt='target' /> </a>
+                    </h1>
+                    <h1 className="link">
+                        <a href="https://www.amazon.com/wedding/victoria-dangelo-john-glennan--october-2022/registry/3JIFEPXJ8PHZN"  alt="a"><img style={{width: "300px", height: "250px"}} src={amazon} alt="amazon" /> </a>
+                    </h1>
+                </div>
         </>
     )
 }
