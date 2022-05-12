e3e4d822985b690d20e32fa2e61f04c47efeb03d
glennanj1 <glennan20@gmail.com>
2022-05-12 19:15:05 -0400

    lower particle count

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -35,14 +35,14 @@ function Home() {
     
       (function frame() {
         confetti({
-          particleCount: 2,
+          particleCount: 1,
           angle: 60,
           spread: 55,
           origin: { x: 0 },
           colors: colors
         });
         confetti({
-          particleCount: 2,
+          particleCount: 1,
           angle: 120,
           spread: 55,
           origin: { x: 1 },
